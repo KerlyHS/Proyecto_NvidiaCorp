5942985426ffdf01bbb1866799c0b361a24147c1
Josue082004 <josue.asanza@unl.edu.ec>
2025-07-14 09:26:16 -0500

    ultimo securyti

--- src/main/java/org/proyecto/nvidiacorp/base/config/SecurityConfig.java
+++ src/main/java/org/proyecto/nvidiacorp/base/config/SecurityConfig.java
@@ -1,19 +1,19 @@
-// package org.proyecto.nvidiacorp.base.config;
+package org.proyecto.nvidiacorp.base.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// public class SecurityConfig {
-//     @Bean
-//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//         http
-//             .csrf().disable()
-//             .authorizeHttpRequests((authz) -> authz
-//                 .anyRequest().permitAll() // Permite acceso público a todas las rutas
-//             );
-//         return http.build();
-//     }
-// }
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().permitAll() // Permite acceso público a todas las rutas
+            );
+        return http.build();
+    }
+}
